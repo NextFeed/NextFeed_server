74766977076bc2005202d1174567357d103b9dd5
yxxshin <samshin3910@snu.ac.kr>
2023-05-21 00:38:20 +0900

    [FEAT] Registered analyze app for admin

--- NextFeed/analyze/admin.py
+++ NextFeed/analyze/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+from .models import Analyze
+from .models import Picture
+
+admin.site.register(User)
+admin.site.register(Analyze)
+admin.site.register(Picture)
